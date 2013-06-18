6dff599bc27645c3f343ab13fcd81fab5c11592a
Valerie Gray <vmgray@email.wm.edu>
2013-06-18 09:44:57 -0400

    Renamed main.cc to HMollerPol.cc

---
+++ HMollerPol.cc
@@ -0,0 +1,20 @@
+/***********************************************************
+Programmer: Valerie Gray
+Purpose:
+
+This is the main function for the H moller Geant4 simulation
+it should put everything together...
+
+Entry Conditions:
+Date: 05-15-2013
+Modified:
+Assisted By:
+*********************************************************/
+
+
+void main ()
+{
+
+  return;
+
+}
