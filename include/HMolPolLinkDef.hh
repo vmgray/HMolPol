29a1dcaee204c5051a65c520ad77caaec5081c04
Valerie Gray <vmgray@email.wm.edu>
2013-07-17 12:27:21 +0200

    This makes sure the Root creates the right dictonaries.

---
+++ include/HMolPolLinkDef.hh
@@ -0,0 +1,23 @@
+/*
+ * HMolPolLinkDef.hh
+ *
+ *  Created on: Jul 16, 2013
+ *      Author: vmgray
+ */
+
+#ifndef HMOLPOLLINKDEF_HH_
+#define HMOLPOLLINKDEF_HH_
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class HMolPolRunInformation+;
+
+#pragma link C++ class HMolPolMainEvent+;
+
+#endif
+
+#endif /* HMOLPOLLINKDEF_HH_ */
