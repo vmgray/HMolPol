e723b43740ee9e7c167b25183cf9d0131a723264
Valerie Gray <vmgray@email.wm.edu>
2013-07-19 14:20:03 +0200

    More hit stuff and ROOT stuff.

--- src/HMolPolEventAction.cc
+++ src/HMolPolEventAction.cc
@@ -1,9 +1,18 @@
-/*
- * HMolPolEventAction.cc
+/********************************************
+ * \author <b>Programmer:</b> Valerie Gray
+ * \author <b>Assisted By:</b> Wouter Deconinck
  *
- *  Created on: Jul 11, 2013
- *      Author: vmgray
- */
+ * \brief <b>Purpose:</b> This is the file is in charge the event action.
+ *      What happens in the beginning and end of an event.
+ *
+ * \date <b>Date:</b> 07-11-2013
+ * \date <b>Modified:</b> 07-18-2013
+ *
+ * \note <b>Entry Conditions:</b>
+ *
+ * \todo comment the comment blocks to get the how this is all put together
+ *
+ ********************************************/
 
 
 // geant4 includes
@@ -17,35 +26,127 @@
 #include "HMolPolEventAction.hh"
 #include "HMolPolGenericDetectorHit.hh"
 
+/********************************************
+ * Programmer: Valerie Gray
+ * Function: HMolPolEventAction
+ *
+ * Purpose: to construct the HMolPolEventAction
+ *
+ * Global:
+ * Entry Conditions: HMolPolAnalysis
+ * Return:
+ * Called By:
+ * Date: 07-11-2013
+ * Modified:
+ ********************************************/
+
 HMolPolEventAction::HMolPolEventAction (HMolPolAnalysis* a)
-: fAnalysis(a)
+: fAnalysis(a),CollID(-1)
 {
 
 }
-
+/********************************************
+ * Programmer: Valerie Gray
+ * Function: ~HMolPolEventAction
+ *
+ * Purpose: to desturct the HMolPolEventAction
+ *
+ * Global:
+ * Entry Conditions:
+ * Return:
+ * Called By:
+ * Date: 07-11-2013
+ * Modified:
+ ********************************************/
 HMolPolEventAction::~HMolPolEventAction()
 {
 
 }
 
+/********************************************
+ * Programmer: Valerie Gray
+ * Function: BeginOfEventAction
+ *
+ * Purpose: What happens at the beginning of a event
+ *
+ * Global:
+ * Entry Conditions: G4Event
+ * Return:
+ * Called By:
+ * Date: 07-11-2013
+ * Modified:
+ ********************************************/
 void HMolPolEventAction::BeginOfEventAction(const G4Event* /*evt*/)
 {
+  //Say that an event is beginning
   G4cout << "At begin of event" << G4endl;
 
-  G4SDManager* SDman = G4SDManager::GetSDMpointer();
+/// \bug not sure what this does at this point forward - it is unused!?!
+//  G4SDManager* SDman = G4SDManager::GetSDMpointer();
 
   // check for existing Collection ID (if it's -1 it will be assigned)
-  if (CollID == -1) {
-      CollID = SDman->GetCollectionID("SD/Collection");
-  }
+  /// \todo reenable hit collection
+//  if (CollID == -1) {
+//      CollID = SDman->GetCollectionID("SD/Collection");
+//  }
 }
 
+/********************************************
+ * Programmer: Valerie Gray
+ * Function: EndOfEventAction
+ *
+ * Purpose: What happens at the end of a event
+ *
+ * Global:
+ * Entry Conditions: G4Event
+ * Return:
+ * Called By:
+ * Date: 07-11-2013
+ * Modified:
+ ********************************************/
 void HMolPolEventAction::EndOfEventAction(const G4Event* evt)
 {
-  G4cout << "At end of event" << G4endl;
+  //We are at the end of an event
+  //  G4cout << "At end of event" << G4endl;
 
   // Get the event number
   fAnalysis->fEvent->fEventNumber = evt->GetEventID();
+
+
+  // Get the hit collections of this event
+  G4HCofThisEvent *HCE = evt->GetHCofThisEvent();
+
+  // Loop over all hit collections, sort by output type
+  for (int hcidx = 0; hcidx < HCE->GetCapacity(); hcidx++)
+  {
+    // Get this particular hit collection
+    G4VHitsCollection* thiscol = HCE->GetHC(hcidx);
+
+    // This is NULL if nothing is stored
+    if (thiscol)
+    {
+      // Dynamic cast to test for types
+      if (HMolPolGenericDetectorHitsCollection* thiscast =
+          dynamic_cast<HMolPolGenericDetectorHitsCollection*>(thiscol))
+      {
+        // Process all hits
+        fAnalysis->fEvent->fGenericDetector.fHits.resize(thiscast->GetSize());
+        for (unsigned int hidx = 0; hidx < thiscast->GetSize(); hidx++) {
+          HMolPolGenericDetectorHit* thisHit =
+              (HMolPolGenericDetectorHit*) thiscast->GetHit(hidx);
+          fAnalysis->fEvent->fGenericDetector.fHits[hidx].fTrackID = thisHit->GetTrackID();
+          fAnalysis->fEvent->fGenericDetector.fHits[hidx].fDetectorID = thisHit->GetDetectorID();
+          fAnalysis->fEvent->fGenericDetector.fHits[hidx].fPosition =
+              TVector3(thisHit->GetPosition().x(),thisHit->GetPosition().y(),thisHit->GetPosition().z());
+          fAnalysis->fEvent->fGenericDetector.fHits[hidx].fMomentum =
+              TVector3(thisHit->GetMomentum().x(),thisHit->GetMomentum().y(),thisHit->GetMomentum().z());
+        }
+      }
+    }
+  }
+
+  //Debugging
+  G4cout << "At end of event number " << evt->GetEventID() << G4endl;
 
   // Fill the tree
   fAnalysis->FillRootTree();
