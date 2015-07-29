0c85918f67166951ed7eacc77376a01fc095a545
vmgray <vmgray@email.wm.edu>
2015-07-29 11:53:47 -0400

    Added Navigation help

--- src/HMolPolGenericDetector.cc
+++ src/HMolPolGenericDetector.cc
@@ -58,7 +58,7 @@ HMolPolGenericDetector::HMolPolGenericDetector(G4String name)
   // Insert name of this detector hits collection into the list of hit collections
   std::stringstream number;
   number << fNumberOfDetectorsWithName[SensitiveDetectorName];
-  G4cout << "Inserting collection " << SensitiveDetectorName + "_" + number.str()
+  G4cout << "  Inserting collection " << SensitiveDetectorName + "_" + number.str()
          << " in SD " << name << "." << G4endl;
   collectionName.insert(SensitiveDetectorName + "_" + number.str());
 
